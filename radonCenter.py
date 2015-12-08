b04e4d3213b8c5dbb39b2677c1959fd65c23c784
Bin Ren <renrenbinbin@gmail.com>
2015-12-07 23:14:39 -0500

    Precison fixed to be 0.01

---
+++ radonCenter.py
@@ -0,0 +1,205 @@
+import numpy as np
+from astropy.io import fits
+from scipy.interpolate import interp2d
+
+def samplingRegion(size_window, theta = [45, 135], m = 0.2, M = 0.8, step = 1, decimals = 1):
+    """This function returns all the coordinates of the sampling region, the center of the region is (0,0)
+    When applying to matrices, don't forget to SHIFT THE CENTER!
+    Input:
+        size_window: the radius of the sampling region. The whole region should thus have a length of 2*size_window+1.
+        theta: the angle range of the sampling region, default: [45, 135] for the anti-diagonal and diagonal directions.
+        m: the minimum fraction of size_window, default: 0.2 (i.e., 20%). In this way, the saturated region can be excluded.
+        M: the maximum fraction of size_window, default: 0.8 (i.e., 80%).
+        step: the seperation between sampling dots (units: pixel), default value is 1pix.
+        decimals: the precisoin of the sampling dots (units: pixel), default value is 0.1pix.
+    Output: (rows, cols)
+        rows: row indecies, flattend.
+        cols: col indecies, flattend.
+    Example:
+        If you call "rows, cols = samplingRegion(5)", you will get:
+        rows: array([ 2.8,  2.1,  1.4,  0.7, -0.7, -1.4, -2.1, -2.8,  2.8,  2.1,  1.4, 0.7, -0.7, -1.4, -2.1, -2.8])
+        cols: array([-2.8, -2.1, -1.4, -0.7,  0.7,  1.4,  2.1,  2.8,  2.8,  2.1,  1.4, 0.7, -0.7, -1.4, -2.1, -2.8])
+    """
+    theta = np.array(theta)
+    zeroDegCols = np.append(np.arange(-int(size_window*M), -int(size_window*m) + 0.1 * step, step), np.arange(int(size_window*m), int(size_window*M) + 0.1 * step, step))
+    #create the column indecies if theta = 0
+    zeroDegRows = np.zeros(zeroDegCols.size)
+    
+    cols = np.zeros((np.size(theta), np.size(zeroDegCols)))
+    rows = np.zeros((np.size(theta), np.size(zeroDegCols)))
+    
+    for i, angle in enumerate(theta):
+        degRad = np.deg2rad(angle)
+        angleDegRows = np.round(zeroDegRows * np.cos(degRad) - zeroDegCols * np.sin(degRad), decimals = decimals)
+        angleDegCols = np.round(zeroDegCols * np.cos(degRad) + zeroDegRows * np.sin(degRad), decimals = decimals)
+        rows[i, ] = angleDegRows
+        cols[i, ] = angleDegCols
+    
+    rows = rows.flatten()
+    cols = cols.flatten()
+
+    return rows, cols
+
+
+def smoothCostFunction(costFunction, halfWidth = 0):
+    """
+    smoothCostFunction will smooth the function within +/- halfWidth, i.e., to replace the value with the average within +/- halfWidth pixel.
+    Input:
+        costFunction: original cost function, a matrix.
+        halfWdith: the half width of the smoothing region, default = 0 pix.
+    Output:
+        newFunction: smoothed cost function.
+    """
+    if halfWidth == 0:
+        return costFunction
+    else:
+        newFunction = np.zeros(costFunction.shape)
+        rowRange = np.arange(costFunction.shape[0], dtype=int)
+        colRange = np.arange(costFunction.shape[1], dtype=int)
+        rangeShift = np.arange(-halfWidth, halfWidth + 0.1, dtype=int)
+        for i in rowRange:
+            for j in colRange:
+                surrondingNumber = (2 * halfWidth + 1) ** 2
+                avg = 0
+                for ii in (i + rangeShift):
+                    for jj in (j + rangeShift):
+                        if (not (ii in rowRange)) or (not (jj in colRange)):
+                            surrondingNumber -= 1
+                        else:
+                            avg += costFunction[ii, jj]
+                newFunction[i, j] = avg * 1.0 / surrondingNumber
+    return newFunction
+    
+def findCostFunctionMax(costFunction, x_centers, y_centers, decimals = 2):
+    interp_costfunction = interp2d(x_centers, y_centers, costFunction, kind='cubic')
+    
+    precision = 0.1
+    x_centers_new = np.round(np.arange(x_ctr_assign - size_cost, x_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    y_centers_new = np.round(np.arange(y_ctr_assign - size_cost, y_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    
+    x_cen = 0
+    y_cen = 0
+    maxcostfunction = 0
+    
+    for x in x_centers_new:
+        for y in y_centers_new:
+            value = interp_costfunction(x, y)
+            if value >= maxcostfunction:
+                maxcostfunction = value
+                x_cen = x
+                y_cen = y
+                
+    precision = 0.01
+    size_cost = 0.1
+    x_ctr_assign = x_cen
+    y_ctr_assign = y_cen
+    x_centers_new = np.round(np.arange(x_ctr_assign - size_cost, x_ctr_assign + size_cost + precision/10.0, precision), decimals=2)
+    y_centers_new = np.round(np.arange(y_ctr_assign - size_cost, y_ctr_assign + size_cost + precision/10.0, precision), decimals=2)
+    
+    maxcostfunction = 0
+    
+    for x in x_centers_new:
+        for y in y_centers_new:
+            value = interp_costfunction(x, y)
+            if value >= maxcostfunction:
+                maxcostfunction = value
+                x_cen = x
+                y_cen = y
+    
+    return x_cen, y_cen
+
+ 
+def searchCenter(image, x_ctr_assign, y_ctr_assign, size_cost = 5, theta = [45, 135], smooth = 2, decimals = 2):
+    """
+    This function searches the center in a grid, 
+    calculate the cost function of Radon Transform (Pueyo et al., 2015), 
+    then interpolate the cost function, 
+    get the center which corresponds to the maximum value in the cost function.
+    
+    Input:
+        image: 2d array.
+        x_ctr_assign: the assigned x-center, or starting x-position; for STIS, the "CRPIX1" header is suggested.
+        x_ctr_assign: the assigned y-center, or starting y-position; for STIS, the "CRPIX2" header is suggested.
+        #size_window: half width of the sampling region; size_window = image.shape[0]/2 - size_cost is suggested.
+        size_cost: search the center within +/- size_cost pixels, i.e., a square region.
+        theta: the angle range of the sampling region; default: [45, 135] for the anti-diagonal and diagonal directions.
+        smooth: smooth the cost function, for one pixel, replace it by the average of its +/- smooth neighbours; defualt = 2.
+        decimals: the precision of the centers; default = 2 for a precision of 0.01.
+    Output:
+        x_cen, y_cen
+    """
+    (y_len, x_len) = image.shape
+
+    x_range = np.arange(x_len)
+    y_range = np.arange(y_len)
+
+    image_interp = interp2d(x_range, y_range, image, kind = 'cubic')
+    #interpolate the image
+    
+    
+    precision = 1
+    x_centers = np.round(np.arange(x_ctr_assign - size_cost, x_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    y_centers = np.round(np.arange(y_ctr_assign - size_cost, y_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    costFunction = np.zeros((x_centers.shape[0], y_centers.shape[0]))
+    #The above 3 lines create the centers of the search region
+    #The cost function stores the sum of all the values in the sampling region
+    
+    size_window = image.shape[0]/2 - size_cost
+    (xs, ys) = samplingRegion(size_window, theta)
+    #the center of the sampling region is (0,0), don't forget to shift the center!
+
+    for j, x0 in enumerate(x_centers):
+        for i, y0 in enumerate(y_centers):
+            value = 0
+            
+            for x1, y1 in zip(xs, ys):
+                x = x0 + x1    #Shifting the center, this now is the coordinate of the RAW IMAGE
+                y = y0 + y1
+            
+                value += image_interp(x, y)
+        
+            costFunction[i, j] = value  #Create the cost function
+
+    smooth = 2
+    costFunction = smoothCostFunction(costFunction, halfWidth = smooth)
+    #Smooth the cost function
+    
+    #x_cen, y_cen = findCostFunctionMax(costFunction, x_centers, y_centers, decimals)
+    interp_costfunction = interp2d(x_centers, y_centers, costFunction, kind='cubic')
+    
+    precision = 0.1
+    x_centers_new = np.round(np.arange(x_ctr_assign - size_cost, x_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    y_centers_new = np.round(np.arange(y_ctr_assign - size_cost, y_ctr_assign + size_cost + precision/10.0, precision), decimals=1)
+    
+    x_cen = 0
+    y_cen = 0
+    maxcostfunction = 0
+    
+    for x in x_centers_new:
+        for y in y_centers_new:
+            value = interp_costfunction(x, y)
+            if value >= maxcostfunction:
+                maxcostfunction = value
+                x_cen = x
+                y_cen = y
+                
+    precision = 0.01
+    size_cost = 0.1
+    x_ctr_assign = x_cen
+    y_ctr_assign = y_cen
+    x_centers_new = np.round(np.arange(x_ctr_assign - size_cost, x_ctr_assign + size_cost + precision/10.0, precision), decimals=2)
+    y_centers_new = np.round(np.arange(y_ctr_assign - size_cost, y_ctr_assign + size_cost + precision/10.0, precision), decimals=2)
+    
+    maxcostfunction = 0
+    
+    for x in x_centers_new:
+        for y in y_centers_new:
+            value = interp_costfunction(x, y)
+            if value >= maxcostfunction:
+                maxcostfunction = value
+                x_cen = x
+                y_cen = y
+    
+       
+    
+    return x_cen, y_cen
